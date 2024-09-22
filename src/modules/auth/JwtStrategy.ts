c2d1a03261bdbedcb22de456ba987cf61a372434
Pedro Muñoz L <pedro.munozlopez@icloud.com>
2024-09-22 17:39:10 -0300

    jwt

--- src/modules/auth/JwtStrategy.ts
+++ src/modules/auth/JwtStrategy.ts
@@ -8,7 +8,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: 'sowar',
+      secretOrKey: 'cachin_cachin',
     });
   }
 
